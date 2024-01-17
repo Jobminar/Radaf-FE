999ddc47c2b2f7e7e3e3292d9a2e2ee3c79b4f4d
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-01-17 17:11:20 +0530

    added PropsList into usermodule and changed the replaced PropSubmit compoennt with lista-property component

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,159 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Aboutus from './components/Aboutus/Aboutus';
-import Support from './components/Support/Support';
-import Valuemyhome from './components/Valuemyhome/Valuemyhome.js';
-import Navbar from './components/Navbar-Footer/Navbar';
-import Home from './components/Home/Home';
-import Footer from './components/Navbar-Footer/Footer';
-import Signup from './components/Profile/signup/Signup.js';
-import Error from './components/Home/Error.js';
-import Viewpage from './components/Profile/MyViewing/Viewpage.js';
-import Listing from './components/Profile/listing/Listing.js';
-import Rentalpage from './components/Profile/Rental/Rentalpage.js';
-import Myaccount from './components/Profile/Myaccount/Myaccount.js';
-import Forsale from './components/Forsale/Forsale.jsx';
-import Tolet from './components/To-let/Tolet.jsx';
-import Myproperty from './components/Myproperty/Myproperty.js';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Aboutus from "./components/Aboutus/Aboutus";
+import Support from "./components/Support/Support";
+import Valuemyhome from "./components/Valuemyhome/Valuemyhome.js";
+import Navbar from "./components/Navbar-Footer/Navbar";
+import Home from "./components/Home/Home";
+import Footer from "./components/Navbar-Footer/Footer";
+import Signup from "./components/Profile/signup/Signup.js";
+import Error from "./components/Home/Error.js";
+import Viewpage from "./components/Profile/MyViewing/Viewpage.js";
+import Listing from "./components/Profile/listing/Listing.js";
+import Rentalpage from "./components/Profile/Rental/Rentalpage.js";
+import Myaccount from "./components/Profile/Myaccount/Myaccount.js";
+import Forsale from "./components/Forsale/Forsale.jsx";
+import Tolet from "./components/To-let/Tolet.jsx";
+import Myproperty from "./components/Myproperty/Myproperty.js";
 // import Propertydetails from './components/Propertydetails/propertydetails.js';
-import Propertydetails from './components/To-let/propertydetails.js';
-import Logout from './components/Profile/profile/Logout.js';
-import Listaproperty from './components/Lista-property/Lista-property.jsx';
-import Login from './components/Profile/profile/Profile.jsx';
-import { AuthProvider } from './components/auth.js';
-import PrivateRoute from './components/PrivateRoute.js';
-
+import Propertydetails from "./components/To-let/propertydetails.js";
+import Logout from "./components/Profile/profile/Logout.js";
+// import Listaproperty from './components/Lista-property/Lista-property.jsx';
+import Login from "./components/Profile/profile/Profile.jsx";
+import { AuthProvider } from "./components/auth.js";
+import PrivateRoute from "./components/PrivateRoute.js";
+import PropSubmit from "./components/PropList/PropSubmit.jsx";
 
 function App() {
-  const isLoggedIn = sessionStorage.getItem('token') !== null;
+  const isLoggedIn = sessionStorage.getItem("token") !== null;
 
   return (
     <AuthProvider>
       <BrowserRouter>
         <Navbar isLoggedIn={isLoggedIn} />
         <Routes>
-          <Route path='*' element={<Error />} />
-          <Route path='/' element={<PrivateRoute><Home /></PrivateRoute>} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/aboutus' element={<PrivateRoute><Aboutus /></PrivateRoute>} />
-          <Route path='/support' element={<PrivateRoute><Support /></PrivateRoute>} />
-          <Route path='/valuemyhome' element={<PrivateRoute><Valuemyhome /></PrivateRoute>} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/listing' element={<PrivateRoute><Listing /></PrivateRoute>} />
-          <Route path='/viewpage' element={<PrivateRoute><Viewpage /></PrivateRoute>} />
-          <Route path='/rentalpage' element={<PrivateRoute><Rentalpage /></PrivateRoute>} />
-          <Route path='/myaccount' element={<PrivateRoute><Myaccount /></PrivateRoute>} />
-          <Route path='/forsale' element={<PrivateRoute><Forsale /></PrivateRoute>} />
-          <Route path='/tolet' element={<PrivateRoute><Tolet /></PrivateRoute>} />
-          <Route path='/listaproperty' element={<PrivateRoute><Listaproperty /></PrivateRoute>} />
-          <Route path='/myproperty' element={<PrivateRoute><Myproperty /></PrivateRoute>} />
-          <Route path='propertydetails' element={<PrivateRoute><Propertydetails /></PrivateRoute>} />
-          <Route path='/logout' element={<PrivateRoute><Logout /></PrivateRoute>} />
-          <Route path='/propertydetails' element={<PrivateRoute><Propertydetails /></PrivateRoute>} />
+          <Route path="*" element={<Error />} />
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/aboutus"
+            element={
+              <PrivateRoute>
+                <Aboutus />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/support"
+            element={
+              <PrivateRoute>
+                <Support />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/valuemyhome"
+            element={
+              <PrivateRoute>
+                <Valuemyhome />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/listing"
+            element={
+              <PrivateRoute>
+                <Listing />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/viewpage"
+            element={
+              <PrivateRoute>
+                <Viewpage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/rentalpage"
+            element={
+              <PrivateRoute>
+                <Rentalpage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/myaccount"
+            element={
+              <PrivateRoute>
+                <Myaccount />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/forsale"
+            element={
+              <PrivateRoute>
+                <Forsale />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/tolet"
+            element={
+              <PrivateRoute>
+                <Tolet />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/listaproperty"
+            element={
+              <PrivateRoute>
+                <PropSubmit />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/myproperty"
+            element={
+              <PrivateRoute>
+                <Myproperty />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="propertydetails"
+            element={
+              <PrivateRoute>
+                <Propertydetails />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/logout"
+            element={
+              <PrivateRoute>
+                <Logout />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/propertydetails"
+            element={
+              <PrivateRoute>
+                <Propertydetails />
+              </PrivateRoute>
+            }
+          />
         </Routes>
         <Footer />
       </BrowserRouter>
---
+++ client/src/components/PropList/SpecialCondition.jsx
@@ -0,0 +1,158 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import {
+  Box,
+  TextField,
+  Checkbox,
+  FormControlLabel,
+  Typography,
+} from "@mui/material";
+
+const SpecialCondition = ({ onChange, values }) => {
+  const [streetParking, setStreetParking] = useState(
+    values.streetParking || false
+  );
+  const [rearGarden, setRearGarden] = useState(values.rearGarden || false);
+  const [gasCentralheating, setGasCentralHeating] = useState(
+    values.gasCentralheating || false
+  );
+  const [doubleGlazed, setDoubleGlazed] = useState(
+    values.doubleGlazed || false
+  );
+  const [Epc, setEpc] = useState(values.Epc || false);
+  const [deleteFlag, setDeleteFlag] = useState(values.deleteFlag || false);
+
+  const handleChange = (name, value) => {
+    if (onChange) onChange(name, value);
+  };
+
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+      {/* Price */}
+      <TextField
+        label="Price"
+        type="number"
+        name="price"
+        value={values.price}
+        onChange={(e) => handleChange("price", e.target.value)}
+      />
+      {/* Place */}
+      <TextField
+        label="Place"
+        type="text"
+        name="place"
+        value={values.place}
+        onChange={(e) => handleChange("place", e.target.value)}
+      />
+      {/* Street Parking */}
+      <FormControlLabel
+        control={
+          <Checkbox
+            name="streetParking"
+            checked={streetParking}
+            onChange={(e) => {
+              setStreetParking(e.target.checked);
+              handleChange("streetParking", e.target.checked);
+            }}
+          />
+        }
+        label="Street Parking"
+      />
+      {/* Rear Garden */}
+      <FormControlLabel
+        control={
+          <Checkbox
+            name="rearGarden"
+            checked={rearGarden}
+            onChange={(e) => {
+              setRearGarden(e.target.checked);
+              handleChange("rearGarden", e.target.checked);
+            }}
+          />
+        }
+        label="Rear Garden"
+      />
+      {/* Gas Central Heating */}
+      <FormControlLabel
+        control={
+          <Checkbox
+            name="gasCentralheating"
+            checked={gasCentralheating}
+            onChange={(e) => {
+              setGasCentralHeating(e.target.checked);
+              handleChange("gasCentralheating", e.target.checked);
+            }}
+          />
+        }
+        label="Gas Central Heating"
+      />
+      {/* Double Glazed */}
+      <FormControlLabel
+        control={
+          <Checkbox
+            name="doubleGlazed"
+            checked={doubleGlazed}
+            onChange={(e) => {
+              setDoubleGlazed(e.target.checked);
+              handleChange("doubleGlazed", e.target.checked);
+            }}
+          />
+        }
+        label="Double Glazed"
+      />
+      {/* Epc */}
+      <FormControlLabel
+        control={
+          <Checkbox
+            name="Epc"
+            checked={Epc}
+            onChange={(e) => {
+              setEpc(e.target.checked);
+              handleChange("Epc", e.target.checked);
+            }}
+          />
+        }
+        label="Epc"
+      />
+      {/* Delete Flag */}
+      <FormControlLabel
+        control={
+          <Checkbox
+            name="deleteFlag"
+            checked={deleteFlag}
+            onChange={(e) => {
+              setDeleteFlag(e.target.checked);
+              handleChange("deleteFlag", e.target.checked);
+            }}
+          />
+        }
+        label="Delete Flag"
+      />
+      {/* Schedule Date Time */}
+      <input
+        type="date"
+        name="scheduleDateTime"
+        value={values.scheduleDateTime}
+        onChange={(e) => handleChange("scheduleDateTime", e.target.value)}
+      />
+      {/* Created On */}
+      const currentDateTimeUK =
+      moment().utcOffset(0).format("YYYY-MM-DDTHH:mm:ss.sssZ");
+      {/* You can access values and other props */}
+      {values && (
+        <Box sx={{ mt: 2 }}>
+          <Typography variant="h6">Values</Typography>
+          <pre>{JSON.stringify(values, null, 2)}</pre>
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+// Define PropTypes
+SpecialCondition.propTypes = {
+  onChange: PropTypes.func,
+  values: PropTypes.object.isRequired,
+};
+
+export default SpecialCondition;
